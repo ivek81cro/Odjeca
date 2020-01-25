96e913cdadd3e3f6c17c33d4cdb121750ce89b4c
Ivan Batinić <ivek81cro@gmail.com>
2020-01-25 23:24:38 +0100

    Add project files.

---
+++ Odjeca/Areas/Admin/Controllers/StoreItemController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Odjeca.Data;
+using Odjeca.Models.ViewModels;
+
+namespace Odjeca.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class StoreItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hostingEnviroment;
+
+        public StoreItemViewModel StoreItemVM { get; set; }
+
+        public StoreItemController(ApplicationDbContext db, IWebHostEnvironment hostingEnviroment)
+        {
+            _db = db;
+            _hostingEnviroment = hostingEnviroment;
+            StoreItemVM = new StoreItemViewModel()
+            {
+                Category = _db.Category,
+                StoreItem = new Models.StoreItem()
+            };
+        }
+        public async Task<IActionResult> Index()
+        {
+            var storeItems = await _db.StoreItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
+            return View(storeItems);
+        }
+
+        //GET-Create
+        public IActionResult Create()
+        {
+            return View(StoreItemVM);
+        }
+    }
+}
---
+++ Odjeca/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Odjeca.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
