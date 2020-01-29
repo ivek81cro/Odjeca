eb9e7451f53f191b47ecbd770de12e845bdae6d7
Ivan Batinić <ivek81cro@gmail.com>
2020-01-29 21:10:19 +0100

    Change register model so new user is by default manager, temporary

--- Odjeca/Utility/SD.cs
+++ Odjeca/Utility/SD.cs
@@ -8,5 +8,9 @@ namespace Odjeca.Utility
     public static class SD
     {
         public const string DefaultClothesImage = "defaultClothes.png";
+        public const string ManagerUser = "Manager";
+        public const string StorageUser = "StorageUser";
+        public const string FrontDeskUser = "FrontDesk";
+        public const string CustomerEndUser = "Customer";
     }
 }
