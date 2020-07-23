d542f98687449df132a6f79abcf03699de2dd596
Ivan Batinić <ivek81cro@gmail.com>
2020-07-23 23:50:30 +0200

    Format fix

--- Odjeca/Areas/Customer/Controllers/CartController.cs
+++ Odjeca/Areas/Customer/Controllers/CartController.cs
@@ -132,7 +132,7 @@ namespace Odjeca.Areas.Customer.Controllers
             DetailCart.OrderHeader.OrderDate = DateTime.Now;
             DetailCart.OrderHeader.UserId = claim.Value;
             DetailCart.OrderHeader.Status = SD.PaymentStatusPending;
-            DetailCart.OrderHeader.PickUpTime = Convert.ToDateTime(DetailCart.OrderHeader.PickUpDate.ToShortDateString() + 
+            DetailCart.OrderHeader.PickUpTime = Convert.ToDateTime(DetailCart.OrderHeader.PickUpDate.ToShortDateString() +
                 " " + DetailCart.OrderHeader.PickUpTime.ToShortTimeString());
 
             List<OrderDetails> orderDetailsList = new List<OrderDetails>();
@@ -184,7 +184,7 @@ namespace Odjeca.Areas.Customer.Controllers
             var service = new ChargeService();
             Charge charge = service.Create(options);
 
-            if(charge.BalanceTransactionId == null)
+            if (charge.BalanceTransactionId == null)
             {
                 DetailCart.OrderHeader.Status = SD.PaymentStatusRejected;
             }
@@ -193,26 +193,26 @@ namespace Odjeca.Areas.Customer.Controllers
                 DetailCart.OrderHeader.TransactionId = charge.BalanceTransactionId;
             }
 
-            if(charge.Status.ToLower() == "succeeded")
+            if (charge.Status.ToLower() == "succeeded")
             {
                 DetailCart.OrderHeader.PaymentStatus = SD.PaymentStatusApproved;
                 DetailCart.OrderHeader.Status = SD.StatusSubmitted;
-
+                 //Send confirmation mail to user
                 var user = await _userManager.GetUserAsync(User);
                 string details = $"<h2>Order number {DetailCart.OrderHeader.Id} details</h2>" +
-                    ("_________________________________________________________________________________________") + 
-                    "<h3>Order detils:</h3>" + 
+                    ("_________________________________________________________________________________________") +
+                    "<h3>Order detils:</h3>" +
                     ("_________________________________________________________________________________________") +
                     ("<table border='0' cellpadding='5' cellspacing='5'>") +
                     ("<tr><th>No.</th><th>Name</th><th>Pcs.</th><th>Price</th></tr>");
                 int i = 1;
-                foreach(var item in DetailCart.listCart)
+                foreach (var item in DetailCart.listCart)
                 {
                     details = details + ($"<tr><td>{i}.</td><td>{item.StoreItem.Name}</td><td>{item.Count}</td><td>{item.StoreItem.Price}$</td></tr>");
                     i++;
                 }
-                details = details + ("</table>") + 
-                    ("_________________________________________________________________________________________") + 
+                details = details + ("</table>") +
+                    ("_________________________________________________________________________________________") +
                     ($"<p><strong>Order total: {DetailCart.OrderHeader.OrderTotal}$</strong></p>") +
                     ($"<p>Payment status: {DetailCart.OrderHeader.PaymentStatus}</p><br />") +
                     ($"<p>You can review order details your account, order history section.</p>");
